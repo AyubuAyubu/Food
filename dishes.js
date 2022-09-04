90dcf9f5bf5213cb918b09557ed3d481837bb2e4
AyubuAyubu <ayubu6143@gmail.com>
2022-09-04 21:17:47 +0300

    Formating our files

--- dishes.js
+++ dishes.js
@@ -1,26 +1,20 @@
-console.log("working")
+console.log("working");
 
-document.addEventListener("DOMContentLoaded",()=>{
-    fetchFood()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  fetchFood();
+});
 
-
-
-function fetchFood(){
-    fetch("http://localhost:3000/dishes")
-      .then(resp=>resp.json())
-     // .then(data=>console.log(data))
-      .then(data => data.forEach(food => getAllFood(food)))
-
+function fetchFood() {
+  fetch("http://localhost:3000/dishes")
+    .then((resp) => resp.json())
+    // .then(data=>console.log(data))
+    .then((data) => data.forEach((food) => getAllFood(food)));
 }
 
-function getAllFood(food ){
-        const parentUl=document.getElementById("food-list")
-        const li=document.createElement("li")
-        li.className="our-dishes"
-        li.innerHTML=`<h1>${food.name}</h1> <p>Ksh ${food.price}</p> <img src="${food.image}"/>`
-        parentUl.appendChild(li) 
-        
-            
+function getAllFood(food) {
+  const parentUl = document.getElementById("food-list");
+  const li = document.createElement("li");
+  li.className = "our-dishes";
+  li.innerHTML = `<h1>${food.name}</h1> <p>Ksh ${food.price}</p> <img src="${food.image}"/>`;
+  parentUl.appendChild(li);
 }
-
